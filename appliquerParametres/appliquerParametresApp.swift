e83370f9bd5d8f540e2fa59c407f65744ac032da
Philippe MICHEL <philippe.michel1@gmail.com>
2023-01-21 13:04:16 +0100

    Initial Commit

---
+++ appliquerParametres/appliquerParametresApp.swift
@@ -0,0 +1,17 @@
+//
+//  appliquerParametresApp.swift
+//  appliquerParametres
+//
+//  Created by Philippe MICHEL on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct appliquerParametresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
