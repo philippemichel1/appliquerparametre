ef6eafda478db6e3f39be0674ba7df20e55873f4
Philippe MICHEL <philippe.michel1@gmail.com>
2023-01-22 21:06:42 +0100

    ajout de couleurs

--- appliquerParametres/Model/Parametres.swift
+++ appliquerParametres/Model/Parametres.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 class Parametres: ObservableObject {
-    static var couleurs = [Color.red ,Color.blue, Color.green, Color.purple, Color.orange]
+    static var couleurs = [Color.red ,Color.blue, Color.green, Color.purple, Color.orange, Color.black, Color.white]
     static var motsMerci = ["merci", "thank you","danke","gracias","grazie","obrigado","дякую." ]
     static var tCaractere = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
     @Published var choixCouleurFond = 0
